dc44a7c62a08477533d5b45b63ee3aec48ffaff0
marek.laskowski <marek@test.com>
2017-11-28 09:45:53 -0500

    parsing the xml, todo: complete the project

--- app/src/main/java/com/example/mareklaskowski/cbc_news_f2017/MainActivity.java
+++ app/src/main/java/com/example/mareklaskowski/cbc_news_f2017/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -61,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
 
             return totalSize; //we don't really use the return value, this is more to satisfice the framework
         }
+        //onPostExecute is called by the framework when the end of doInBackground is reached
+        protected void onPostExecute(Long result){
+            //once finished all we do here is print out the headlines..
+            //or exted this to refresh any views you have (if necessary)
+            Log.d("ASYNCTASK COMPLETE", "Downloaded " + result + "files");
+        }
     }
 
     //all our work will go into this method
@@ -74,9 +83,10 @@ public class MainActivity extends AppCompatActivity {
             InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
             //let's do something with the data!
 
+
+            //for now, to test things let's create a buffered reader and just display the contents of the file
+
             /*
-            for now, to test things let's create a buffered reader and just display the contents of the file
-            */
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             //create a string variable to store each line at a time as we read it from bufferedReader
             String inputLine = null;
@@ -85,9 +95,52 @@ public class MainActivity extends AppCompatActivity {
                 //display for debugging purposes
                 Log.d("HEADLINES DOWNLOADED", inputLine);
             }
-
-
+            */
             //2) parse
+            //now that we have downloaded the file, use the XMLPullParser API to parse the xml
+            //and extract the information we need
+            //first, get an instance of XmlPullParser from XmlPullParserFactory (recall the factory pattern)
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            //configure the factory to create the specific xml parser flavour we will use
+            factory.setNamespaceAware(true);
+            //get the XmlPullParser instance from the factory
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            //connect the XmlPullParser instance to the data source
+            //note: we can pass a variety of standard Java IO classes here
+            xmlPullParser.setInput(inputStreamReader);
+            //recall that the XmlPullParser communicates with your code using "Events"
+            int event = xmlPullParser.getEventType();//call this every time we want to inspect the event that occurred
+
+            //pattern: use a flag to recall that we are inside a particular xml element, for example the title element
+            boolean insideTitle = false;//we're at the start of the docuemnt to begin with, not inside the title
+            while(event != XmlPullParser.END_DOCUMENT){
+                //process events/elements inside this loop
+                if(event == XmlPullParser.START_DOCUMENT){//this will be the case when the start of the document is reached
+                    Log.d("PARSING XML", "We reached the start of the document");
+                }else if(event == XmlPullParser.START_TAG){//case when the start of an element (tag) is reached
+                    String tagName = xmlPullParser.getName(); //gets the element type
+                    Log.d("PARSING XML", "We reached the start of tag: " + tagName);
+                    //TODO: use the tag name or type here to determine whether you have reached a news <item> or <link>
+                    if(tagName.equalsIgnoreCase("title")){
+                        //store this headline in the headlines
+                        Log.d("PARSING XML", "found a title tag:" + tagName);
+                        insideTitle = true;
+                    }
+                }else if(event == XmlPullParser.END_TAG){//encountered the end of an element
+                    Log.d("PARSING XML", "We reached the end of tag: " + xmlPullParser.getName());
+                    insideTitle = false;
+
+                }else if(event == XmlPullParser.TEXT){//encountered the CONTENT of an element
+                    String text = xmlPullParser.getText();
+                    Log.d("PARSING XML", "found text: " + text);
+                    if(insideTitle){
+                        //add the headline to the list of headlines...
+                    }
+                }
+
+                //don't forget to get the next event or element in the xml file!
+                event = xmlPullParser.next();//bit like navigating SQL records...
+            }
 
         }catch (Exception e){
             Log.e("DOWNLOAD OR PARSE ERROR", e.getMessage());
